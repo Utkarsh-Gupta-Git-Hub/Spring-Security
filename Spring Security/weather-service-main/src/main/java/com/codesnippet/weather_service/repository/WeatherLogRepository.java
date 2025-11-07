e3e91286e256438d8da4c36448fb1be00045d7cc
Utkarsh Gupta <143832160+Utkarsh-Gupta-Git-Hub@users.noreply.github.com>
2025-11-07 22:52:10 +0530

    Delete Spring Security/weather-service-main/src/main/java/com/codesnippet/weather_service/repository/WeatherLogRepository.java

--- Spring Security/weather-service-main/src/main/java/com/codesnippet/weather_service/repository/WeatherLogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codesnippet.weather_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeatherLogRepository extends JpaRepository<WeatherLog, Long> {
-}
